32b3d8d13faa8eea1685ec4381ffa14465cf5e40
Kent Martin Åsvang <kent@asvang.no>
2022-12-04 21:59:08 +0100

    cleaned up boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
+  <h1> home view </h1>
+  <HelloWorld />
+
 </template>
